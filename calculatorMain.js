de06c53920441496119c16a872dea449c85f1488
asmita85 <49493424+asmita85@users.noreply.github.com>
2020-06-27 00:11:33 -0500

    First Commit

---
+++ calculatorMain.js
@@ -0,0 +1,24 @@
+function calc() {
+    //grap the value of the first input parseInt to convert the data to int 
+    var a = parseInt(document.querySelector("#value1").value);
+    //grap the value of the second input 
+    var b = parseInt(document.querySelector("#value2").value);
+    //grap the value of the operator  input no need to convert it to num
+    var op = document.querySelector("#operator").value;
+    var calculate;
+
+    //calculate
+    if (op == "add"){
+        calculate = a + b;
+    }else if (op == "min"){
+        calculate = a - b;
+    }else if (op == "div"){
+        calculate = a / b;
+    }else if (op == "mul"){
+        calculate = a * b;
+    }
+    
+    document.querySelector("#result").innerHTML = calculate;
+
+
+}
